9e04291f6e78a2a4f8ea14b8a45d16d285ef7747
Juan Pablo <juan.yarce1@udea.edu.co>
2024-01-24 00:32:07 -0500

    progress

--- prueba-tecnica/App.jsx
+++ prueba-tecnica/App.jsx
@@ -1,18 +1,50 @@
 import { useEffect, useState } from "react"
 
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+//const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}`
+
 export function App(){
-    const[fact,setFact] = useState('asjdoasd')
+    const[fact,setFact] = useState()
+    const[imageUrl,setImageUrl] = useState()
 
     useEffect(() => {
-        fetch('https://catfact.ninja/fact')
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
         .then(res => res.json())
-        .then(data => setFact(data.fact))
+        .then(data => {
+            const {fact} = data
+            setFact(fact)
+
+            const firstWord = fact.split(' ')[0]
+            console.log(firstWord)
+            //console.log(`https://cataas.com/cat/says/${firstWord}`)
+
+            fetch(`https://cataas.com/cat/says/${firstWord}`)
+                .then(res =>{
+                    res.json()
+                    console.log(res, 'json')
+                })
+                .then(response => {
+                    console.log("Holi");
+                    console.log(response.url, "url");
+                  //  setImageUrl(`https://cataas.com${url}`);
+                    console.log("Nuevo valor de imageUrl:", imageUrl);
+
+                    // Puedes realizar acciones adicionales aquí, ya que este bloque se ejecuta después de que el estado se haya actualizado.
+                })
+                .catch(error => {
+                    console.error("Error al obtener la imagen:", error);
+                });
+        });
+        
     }, [])
+
+
 
     return (
         <main>
             <h1>App gatos</h1>
-            <p>{fact}</p>
+            {fact && <p>{fact}</p>}
+            {imageUrl && <img src={imageUrl}/>}
         </main>
     )
 }
